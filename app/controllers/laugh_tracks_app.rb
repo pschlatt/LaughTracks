41dd655bfba7de2d38d1063ac938b671488eac7a
Paul Schlattmann <paul.h.schlattmann@gmail.com>
2019-03-18 09:17:03 -0600

    Adds Input Comedian Field

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -6,4 +6,15 @@ get '/comedians' do
   erb :index
 end
 
+get '/comedians/new' do
+  erb :new
+end
+
+post '/comedians' do
+    Comedian.create(name: params[:comedian][:name],
+                    age: params[:comedian][:age],
+                    birthplace: params[:comedian][:birthplace])
+    redirect '/comedians'
+  end
+
 end
